7d55362c48ff0c4d375ecf07bfd0f3e18e47c0bd
sdahal-dev <sdahal.contact@gmail.com>
2025-08-16 15:37:24 -0500

    Add Search on Desktop

--- src/Components/Navbar/DesktopNavbar.jsx
+++ src/Components/Navbar/DesktopNavbar.jsx
@@ -1,8 +1,12 @@
 import sinemaLogo from "../../assets/sinema-logo.png";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid"; // or /outline
 
 const DesktopNavbar = () => {
+  const [searchBar, setSearchBar] = useState(false);
+  const [exitSearch, setExitSearch] = useState(true);
   return (
     <div className="desktop-navbar-wrapper flex w-screen fixed justify-between items-center text-sm font-semibold text-white py-2 border-b">
       {/* Logo & Name */}
@@ -11,29 +15,57 @@ const DesktopNavbar = () => {
         <h1>Sinema</h1>
       </div>
 
-      {/* Desktop Navbar Links */}
-      <div className="navbar-links">
-        <NavLink to="/" className="links ml-10 mr-6 font-bold">
-          Home
-        </NavLink>
+      {/* Search Bar */}
 
-        <NavLink to="/movies" className="links ml-10 mr-6 font-bold">
-          Movies
-        </NavLink>
+      {searchBar ? (
+        <>
+          {exitSearch ? (
+            <>
+              <div className="search-bar-set relative">
+                <input
+                  type="search"
+                  placeholder="Search..."
+                  className="py-1 pr-8 px-4 rounded-3xl border focus:outline-none w-70"
+                />
+                <XMarkIcon
+                  className="h-5 w-5 absolute right-2 top-1 ml-5 hover:cursor-pointer"
+                  onClick={() => setSearchBar(false)}
+                />
+              </div>
+            </>
+          ) : null}
+        </>
+      ) : (
+        /* Desktop Navbar Links */
+        <div className="navbar-links">
+          <NavLink to="/" className="links ml-10 mr-6 font-bold">
+            Home
+          </NavLink>
 
-        <NavLink to="/favorites" className="links ml-10 mr-6 font-bold">
-          Favorites
-        </NavLink>
+          <NavLink to="/movies" className="links ml-10 mr-6 font-bold">
+            Movies
+          </NavLink>
 
-        <NavLink to="/search" className="links ml-10 mr-6 font-bold">
-          Search
-        </NavLink>
-      </div>
+          <NavLink to="/favorites" className="links ml-10 mr-6 font-bold">
+            Favorites
+          </NavLink>
+
+          <a
+            to="/search"
+            className="links ml-10 mr-6 font-bold"
+            onClick={() => setSearchBar(true)}
+          >
+            Search
+          </a>
+        </div>
+      )}
 
       {/* Sign In / Log Out */}
-      <div className="signing border-2 border-white mr-5 py-1 px-4 rounded-3xl text-white bg-[rgba(220,144,50,0.64)] hover:bg-white hover:text-[rgba(220,144,50,0.64)] transition-all duration-300 ease-in-out">
-        <button className="cursor-pointer">Sign In</button>
-      </div>
+      <NavLink to="/login">
+        <div className="signing border-2 border-white mr-5 py-1 px-4 rounded-3xl text-white bg-[rgba(220,144,50,0.64)] hover:bg-white hover:text-[rgba(220,144,50,0.64)] transition-all duration-300 ease-in-out">
+          <button className="cursor-pointer">Sign In</button>
+        </div>
+      </NavLink>
     </div>
   );
 };
